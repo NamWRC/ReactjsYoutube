7c14b32444ae03703cbe86bafd346d2b592a3643
NamWRC <nam.lh.61cntt@ntu.edu.vn>
2023-11-10 13:24:22 +0700

    head style done

--- clone-youtube/src/component/search/Search.js
+++ clone-youtube/src/component/search/Search.js
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faMagnifyingGlass,
     faMicrophone,
+    faKeyboard,
 } from "@fortawesome/free-solid-svg-icons";
 
 const Search = () => {
+    const [searching, setSearching] = useState(false);
     return (
         <div className="search-header">
             <div className="search-input-container">
-                <div className="search-hidden">
+                <div
+                    className={`search-hidden ${
+                        searching ? "active-search" : ""
+                    }`}
+                >
                     <button>
                         <FontAwesomeIcon
                             icon={faMagnifyingGlass}
                         ></FontAwesomeIcon>
                     </button>
                 </div>
-                <input type="text" className="search-input" />
+                <div
+                    className={`search-box ${searching ? "active-input" : ""}`}
+                >
+                    <input
+                        type="text"
+                        className="search-input"
+                        placeholder="Search"
+                        onFocus={() => setSearching(true)}
+                        onBlur={() => setSearching(false)}
+                    />
+                    <div className="search-keyboard">
+                        <FontAwesomeIcon icon={faKeyboard}></FontAwesomeIcon>
+                    </div>
+                </div>
 
                 <div className="search-btn">
                     <button>
